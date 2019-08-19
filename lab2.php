0c018b8cc2a9921e4c815cb28794eda8aeffdd77
eev417 <54092322+eev417@users.noreply.github.com>
2019-08-18 18:19:10 -0700

    Update lab2.php

--- lab2.php
+++ lab2.php
@@ -1,10 +1,10 @@
-
 <!DOCTYPE html>
 <html>
-  <head>
+	<head>
     <title>Lab 2</title>
-  </head>
-    <p>Page 1 is the login/registration page.</p>
+    </head>
+	<body>
+		<p>Page 1 is the registration/login page.</p>
 	<br>
 	<p>Page 2 is the user creation successful page.</p>
 	<br>
@@ -14,62 +14,80 @@
 	<br>
 	<p>Page 5 is the failure to login/account creation page.</p>
 	<br>
-    <?php
-      $pgNum = 0;
-      if(isset($_GET["page"])){
-        $pageNum = $_GET["page"];
-          settype($pageNum, "integer");
-      }
-      else{
-        $pageNum = 1;
-      }
-      if($pageNum > 5 || $pageNum < 1){
-        echo("<strong>Error...this page does not exist!</strong></body></html>");
-        die();
-      }
+	
+		
+		<?php
+			$pageNum = 0;
+			if( isset($_GET["page"]) )
+			{
+				$pageNum = $_GET["page"];
+				settype($pageNum, "integer");
+			}
+			else
+			{
+				$pageNum = 1;
+			}
 
-      echo ("<strong>This is page# $pageNum</strong>");
-        if($pageNum == 1){
-    ?>
-      <form method="get"
-action="<?php echo $_SERVER['localhost']; ?>">
+			if($pageNum > 2 || $pageNum < 1)
+			{
+				echo("<strong>Error...this page does not exist!</strong></body></html>");
+				die();
+			}
+
+			echo ("<strong>This is page# $pageNum</strong>");
+			if($pageNum == 1)
+			{
+		?>
+			<form method="get" action="">
 
           <input type="hidden" name="page" value="2">
+		  <input type="hidden" name="signup" value="1">
           <p><label>Username
             <input type="text" name="username">
           </label></p>
           <p><label>Password
             <input type="text" name="password">
           </label></p>
-          <p><input type="submit" value="SUBMIT"></p>
+		  <p><label>Retype Password
+		    <input type="text" name="repassword">
+		  </label></p>
+          <p><input type="submit" name ="submit" value="REGISTER"></p>
         </form>
-      <?php
-        } else if {//$pgNum == 2
-            $username = $password = "";
-            if (isset($_GET["username"]
-              && $_GET["username"] != ""
-              && isset($_GET["password"]
-              && $_GET["password"] != ""){
-              $username = $_GET["username"];
-              $password = $_GET["password"];
-            }
-            else{
+		<?php
+			} else { // $pageNum == 2
+			$dbusername = $dbpassword = "";
+				if (isset($_GET["username"]) && isset($_GET["password"])){
+					$dbusername = $_GET["username"];
+					$dbpassword = $_GET["password"];
+					$dbsalt = $_GET["repassword"];
+					$con = mysqli_connect("localhost","root","maria484","users");
+					if (mysqli_connect_errno()){
+					echo "Failed to connect to db." . mysqli_connect_errno();	
+					}
+					//$query = "INSERT INTO user ('userid', 'username', 'password', 'salt') VALUES (NULL, 
+					mysqli_query($con, "INSERT INTO user ('userid','username','pass','salt')
+					VALUES (NULL, '$dbusername', '$dbpassword', '$dbsalt')");
+					mysqli_close($con);
+						
+				}
+				else
+				{
+					echo("<p><strong>Error...your info was not received properly...!</strong></p></body></html>");
+					die();
+				}
+		?>
+			<p> This is your username: <?php echo $dbusername; ?>.</p>
+			<p> This is your password: <?php echo $dbpassword; ?>.</p>
 
-              echo("<p><strong>Error...your info was not received properly...!</strong></p></body></html>");
-              die();
-            }
-}
-        ?>
-      <p> This is your username: <?php echo $username; ?>.</p>
-      <p> This is your password: <?php echo $password; ?>.</p>
-        <form method="get"
-          action="<?php echo $_SERVER['PHP_SELF']; ?>">
-            <input type="hidden" name="page" value="1">
-        <p><input type="submit" value="Back to Page 1"></p>
-                        </form>
+			<form method="get" action="">				
+				<input type="hidden" name="page" value="1">
+				<p><input type="submit" value="Back to Page 1"></p>
+			</form>
+			
+		<?php
+			}
+		?>
 
-                <?php
-                        }
-                ?>
-  </body>
+		<hl><br><br><br>
+	</body>
 </html>
